3becb3c8175809c6d2ebaceadf8e4564a9470fcb
caleblareau <caleblareau@g.harvard.edu>
2016-12-21 22:16:03 -0600

    tweaks to faciliate chromVAR 0.3

--- code/deviationCompute.R
+++ code/deviationCompute.R
@@ -1,83 +1,36 @@
 #+ echo=FALSE, message=FALSE, warning=FALSE
-library(readr)
 library(chromVAR)
 library(Matrix)
-library(BSgenome.Hsapiens.UCSC.hg19)
 library(GenomicRanges)
-library(plotly)
-library(heatmaply)
+library(diffloop)
 
 if (basename(getwd()) != "code") setwd("code")
 
 # For laptop
 BiocParallel::register(BiocParallel::MulticoreParam(2, progressbar = FALSE))
 
-#' # Analysis of Scadden Cell Paper
-# Performed by Caleb Lareau, 4 December
+load("../output/peaksCounts.rda")
+types <- readRDS("../output/sampleTypes.rds")
+sample_annotation <- DataFrame(type = types)
+hemedf <- SummarizedExperiment(assays = list(counts = Matrix(counts)), rowRanges = peaks, colData = sample_annotation)
 
-#+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
-csv <-  "../data/scadden_counts.csv"
-counts <- Matrix(data.matrix(read_csv(csv)))
-peakdf <- read.table("../data/peaks.bed")
-names(peakdf) <- c("chr", "start", "end")
-peaks <- GRanges(peakdf)
+# Read GWAS hits in and process ||| BED FILE
+hitdf <- read.table("../data/GWAS/20160617_both_sig_MPRA_SNPs.bed")
+names(hitdf) <- c("chr", "start", "end", "snp")
+hitG <- GRanges(hitdf)
 
-peaks <- get_gc(peaks, genome = BSgenome.Mmusculus.UCSC.mm10)
-praw <- peaks
-peaks <- sort(peaks)
-counts <- counts[match(peaks,praw),]
+# Read two lines
+hitdf <- read.table("../data/GWAS/sarc.ea.10e-04.txt")
+hitdf$V3 <- hitdf$V2
+names(hitdf) <- c("chr", "start", "end")
+hitG <- addchr(GRanges(hitdf))
 
-#' ## Get motifs; compute deviations. 
-#+ cache = TRUE, message=FALSE, warning=FALSE, echo = FALSE
-bg <- get_background_peaks(counts_mat = counts, bias = peaks)
-# 
-# # chromVAR motifs
-# #motifs <- get_motifs(species = "Mus musculus")
-# load our own
-load("../data/cisbp_mm9_unique.08_Jun_2016.RData")
+# Build matches index
+ov <- findOverlaps(peaks, hitG)
+ix <- Matrix(as.integer(1:length(peaks) %in% unique(queryHits(ov))))
+ixx <- SummarizedExperiment(assays = list(match = Matrix(ix)), rowRanges = peaks,  colData = DataFrame(hit = "GWAS"))
 
-motif_ix <- match_pwms(pwms, peaks, genome = BSgenome.Mmusculus.UCSC.mm10)
-motif_score <- match_pwms(pwms, peaks, genome = BSgenome.Mmusculus.UCSC.mm10, out = "scores")
-deviations <- compute_deviations(counts_mat = counts, background_peaks = bg, peak_indices = motif_ix)
-saveRDS(deviations, file = "../data/deviations.rds")
+deviations <- compute_deviations(hemedf, background_peaks = bg, annotations = ixx)
 
-deviations <- readRDS("../data/deviations.rds")
-variability <- compute_variability(deviations$z)
-labels <- TFBSTools::name(pwms[rownames(variability)])
-
-#namesdf <- read.table("../data/libnames.txt", header = FALSE)
-#namesvec <- as.character(namesdf[,2])
-#names(namesvec) <- namesdf[,1]
-
-#Kmer stuff
-# kmer_idx <- get_kmer_indices(peaks, genome = BSgenome.Mmusculus.UCSC.mm10)
-#write.table(data.frame(data.matrix(kmer_idx)), file = "../data/161119_peaksXkmer.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
-
-## View plots of variable TFs and deviation heatmap
-#+ fig.width=7, fig.height=7, message = FALSE, warning = FALSE, echo=FALSE
-#plot_variability(variability, labels = labels)
-boo <- which(variability$p_value_adj<10^-100)
-length(boo)
-#write.table(data.frame(data.matrix(motif_score)), file = "../data/161207_peaksXscores.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
-#write.table(data.frame(data.matrix(motif_ix)), file = "../data/161207_peaksXmatch.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
-write.table(data.frame(data.matrix((deviations$z))), file = "../data/161207_motifsXsamples_zscores.tsv", sep = "\t", row.names = TRUE, quote = FALSE)
-
-#' ## View TF x Sample clusters
-#+ fig.width=7, fig.height=7, message = FALSE, warning = FALSE, echo=FALSE
-z <- deviations$z
-colnames(z) <- as.character(namesvec[colnames(z)])
-df <- z[boo,]
-df2 <- df
-df2[df2 > 5] <- 5
-df2[df2 < -5] <- -5
-heatmaply(df, scale_fill_gradient_fun = ggplot2::scale_fill_gradient2(low = "blue", high = "red"), limits = c(-5, 5))
-
-#' ## View clusters of TF
-#+ fig.width=7, fig.height=7, message = FALSE, warning = FALSE, echo=FALSE
-tfcor <- cor(t(df))
-heatmaply(tfcor, scale_fill_gradient_fun = ggplot2::scale_fill_gradient2(low = "blue", high = "red"))
-
-#' ## View clusters of samples
-#+ fig.width=7, fig.height=7, message = FALSE, warning = FALSE, echo=FALSE
-samplecor <- cor(df)
-heatmaply(samplecor, scale_fill_gradient_fun = ggplot2::scale_fill_gradient2(low = "blue", high = "red"))
+rawdf <- data.frame( types = types, zscore = unname(assays(deviations)$z)[1,])
+aggregate(rawdf, by = list(Cell = rawdf$types), FUN = mean)[,c(1,3)]
